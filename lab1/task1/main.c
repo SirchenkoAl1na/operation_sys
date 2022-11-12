a0c2d3121a0ff1cc3034c8db74b63e93844a7c33
Alina <sichenko22.as@gmail.com>
2022-11-12 12:41:00 +0200

    Unarchive lab_1

---
+++ lab1/task1/main.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/wait.h"
+
+int main() {
+    int pid0 = fork();
+
+    if (pid0 == 0) {
+        fork();
+        sleep(30);
+    } else {
+        int pid2 = fork();
+
+        if (pid2 == 0) {
+            int pid3 = fork();
+            if (pid3 != 0) {
+	        fork();
+	    }
+            sleep(30);
+        } else {
+            fork();
+            sleep(30);
+        }
+    }
+    return 0;
+}
